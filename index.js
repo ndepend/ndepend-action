6551b5f41a7ce2ae9a6b9b29ad7bfa8bcbea48ac
CoderGears <support@codergears.com>
2024-11-21 11:32:28 +0100

    baseline

--- index.js
+++ index.js
@@ -447,10 +447,7 @@ async function run() {
         if(message.indexOf("at least one Quality Gate failed")>0 && stopifQGfailed=='true')
           core.setFailed("The NDepend action failed the build because at least one Quality Gate failed and stopIfQGFailed is set to true  in the action options.");
 
-      }
-
-     
-      
+      }     
       
       } catch (error) {
         core.setFailed(error.message);
